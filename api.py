13e7da1332b7fb5655b42c25bcf5d1c20325e1ec
Celia Prieto <cprietosegura@gmail.com>
2019-12-17 14:49:57 +0100

    desarrollando la api

---
+++ api.py
@@ -0,0 +1,37 @@
+from flask import Flask
+import pickle
+import numpy as np
+import os
+#import cv2
+from src.prediction import prediction
+from werkzeug import secure_filename
+from flask import Flask, render_template,request,  jsonify
+
+# instancia del objeto Flask
+app = Flask(__name__)
+
+# instancia del objeto Flask
+app.config['UPLOAD_FOLDER'] = './diagnosis'
+
+@app.route("/")
+def upload_file():
+ # renderiamos la plantilla "formulario.html"
+ return render_template('index.html')
+
+@app.route("/upload", methods=['POST'])
+def uploader():
+ if request.method == 'POST':
+  # obtenemos el archivo del input "archivo"
+  f = request.files['archivo']
+  filename = secure_filename(f.filename)
+  # Guardamos el archivo en el directorio "Archivos PDF"
+  f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+  # Retornamos una respuesta satisfactoria
+  pred = prediction(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+  return jsonify(pred)
+  #"<h2>Archivo subido exitosamente</h2>"
+
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ src/prediction.py
@@ -0,0 +1,32 @@
+import pickle
+import numpy as np
+import cv2
+
+def loadTrainedModel():
+    filepath='./output/models/rfc_model_acu81.sav'
+    loaded_model = pickle.load(open(filepath, 'rb'))
+    return loaded_model
+
+def resizeImages(img_path,size=(50,50)):
+    """resize images to 50x50 and format to prediction"""
+    image = cv2.imread(img_path)
+    resized = cv2.resize(image, size, interpolation=cv2.INTER_CUBIC)
+    img=np.array([resized.flatten()])
+    #print('resized image')
+    return img
+
+def predictNewImage(loaded_model,path):
+    im = resizeImages(path)
+    pred_nueva=loaded_model.predict(im)
+    #print(pred_nueva)
+    if pred_nueva == 0:
+        diagnosis={'Diagnosis':'Benign tumor'}
+    else:
+        diagnosis={'Diagnosis':'Malignant tumor'}
+    
+    return diagnosis 
+
+def prediction(filepath):
+    loaded_model= loadTrainedModel()
+    prediction=predictNewImage(loaded_model,filepath)
+    return prediction
